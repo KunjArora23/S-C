9ad4a8f92924749755b7ab79f714392756a037db
KunjArora23 <141988347+KunjArora23@users.noreply.github.com>
2026-05-04 21:23:54 +0530

    Update api.js

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://sandctours.duckdns.org/api/v1';
+const API_BASE_URL = 'https://sandctours.duckdns.org';
 
 // Create axios instance
 const axiosInstance = axios.create({
