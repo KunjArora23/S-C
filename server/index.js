7205ed140f4c251c98ce272b7b31c4d1b088f33e
KunjArora23 <141988347+KunjArora23@users.noreply.github.com>
2026-05-13 08:49:01 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ import cors from 'cors';
 dotenv.config();
 
 const app = express();
-const CORS_ORIGINS = ["https://www.sandctours.com", "https://sandctours.com/"]
+const CORS_ORIGINS = ["https://www.sandctours.com", "https://sandctours.com"]
 
 // app.use((req, res, next) => {
 //     console.log("Incoming:", req.method, req.url);
